445edf0015c0f896173f0cb4c8deb02b97eb9211
Marija Kalebota Kodzoman <marija.kalebota@gmail.com>
2019-07-26 10:22:40 -0700

    Add dice-rolling functionality. Modify index to reflect this change.

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+import random
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    greeting = "Welcome to the dice rolling app!<br><br>"
+    greeting+= "Options:<br>"
+    greeting+= "\"/roll/<i>die_size</i>\" - Rolls a die of size <i>die_size</i>"
+
+    return greeting
+
+@app.route('/roll/<die_size>')
+def roll_size(die_size):
+    return str(random.randint(1, int(die_size)))
