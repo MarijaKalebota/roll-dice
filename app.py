985bf6e09ba71c96f1df6b9bf7bee516e28bd66b
Marija Kalebota Kodžoman <marija.kalebota@gmail.com>
2019-07-31 14:52:57 -0700

    Sync "testing" branch with master (#4)
    * Add dice-rolling functionality. Modify index to reflect this change. (#2)
    
    * Add dice-rolling functionality. Modify index to reflect this change.
    
    * Catch exception in die-rolling functionality. Modify imports.
    
    * Update requirements for testing purposes (#3)

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import random
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    greeting = "Welcome to the dice rolling app!<br><br>"
+    greeting+= "Options:<br>"
+    greeting+= "\"/roll/<i>die_size</i>\" - Rolls a die of size <i>die_size</i>"
+
+    return greeting
+
+@app.route('/roll/<die_size>')
+def roll_size(die_size):
+    try:
+        return str(random.randint(1, int(die_size)))
+    except:
+        return "There was an error! Make sure you are inputting the size of the die as a number."
+
